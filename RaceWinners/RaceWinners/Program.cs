240217fd6f15b1ea08dd47eff173cfe42d41f0f6
Aidan Leyden <aidanleyden@gmail.com>
2021-09-22 20:38:01 -0400

    quasi
    motkio

--- RaceWinners/RaceWinners/Program.cs
+++ RaceWinners/RaceWinners/Program.cs
@@ -22,14 +22,33 @@ namespace RaceWinners
                 Console.WriteLine($"{data[i].Name} - [{ranks}]");
             }
 
-            double[] z = { };
+            double[] z = new double[4];
             for (int i = 0; i < data.Count; i++)
             {
                 double k = data[i].Ranks.Take(7).Sum();
                 k /= 7;
+                z[i] = k;
                 Console.WriteLine(k);
-                
             }
+
+            int[] ranked = new int[4];
+            for (int i = 0; i < 4; i++) {
+                int curRank = 1;
+
+                for (int o = 0; o < i; o++) {
+                    if (z[i] > z[o]) {
+                        curRank++;
+                    } else {
+                        ranked[o]++;
+                    }
+                }
+
+                ranked[i] = curRank;
+            }
+            Console.WriteLine("class A rank:" + ranked[0]);
+            Console.WriteLine("class B rank:" + ranked[1]);
+            Console.WriteLine("class C rank:" + ranked[2]);
+            Console.WriteLine("class D rank:" + ranked[3]);
         }
     }
 }
